1d899316afd42767e90f783c980d6c074849e7e9
Lindsay Miller <lmm566@gmail.com>
2018-05-28 23:47:14 -0400

    Adds img and google fonts

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,42 @@
+//Shuffle icons
+let icons = [
+    '<i class="fas fa-paw"></i>',
+    '<i class="fab fa-pagelines"></i>',
+    '<i class="fas fa-bug"></i>',
+    '<i class="fas fa-dove"></i>',
+    '<i class="fas fa-frog"></i>',
+    '<i class="fas fa-leaf"></i>',
+    '<i class="fas fa-kiwi-bird"></i>',
+    '<i class="fas fa-tree"></i>',
+    '<i class="fas fa-paw"></i>',
+    '<i class="fab fa-pagelines"></i>',
+    '<i class="fas fa-bug"></i>',
+    '<i class="fas fa-dove"></i>',
+    '<i class="fas fa-frog"></i>',
+    '<i class="fas fa-leaf"></i>',
+    '<i class="fas fa-kiwi-bird"></i>',
+    '<i class="fas fa-tree"></i>'
+]
+
+shuffle(icons);
+
+$(".back").each(function (i){
+    $(this).append(icons[i]);
+});
+
+
+//Set starting values for variables
+
 let activeCards = 0;
 let checkingState = false;
 let moveCounter = 0;
-document.getElementById('moveCounter').innerHTML = moveCounter;
+document.getElementById('numberMoves').innerHTML = moveCounter;
 let starRating = 3;
-document.getElementById('starRating').innerHTML = starRating;
+// document.getElementById('starRating').innerHTML = starRating;
 let numberMatched=$(".matched").length;
 let matchesPerMove = numberMatched/moveCounter;
+let seconds = 0;
+let timeVar;
 
 //Reload when click reset button
 
@@ -13,15 +44,22 @@ $('#resetButton').click(function() {
     location.reload();
 });
 
+//Close modal
+
+$("#modalButton").click(function(){
+    location.reload();
+    $("#winnerModal").hide();   
+})
+
 //Listen to click on card element
 
 $(".card").click(function(){
 
-    //Increase move counter 
-    moveCounter += 1;
-    document.getElementById('moveCounter').innerHTML = moveCounter;
-    console.log("The number of moves is " + moveCounter);
-    
+    //If first click, start timer
+    if (moveCounter === 0 ){
+        timeVar = setInterval(myTimer, 1000);
+        seconds = 0;
+    }
 
     //If card already flipped, throw error to log
     if(!$(".back",this).hasClass("d-none")){
@@ -33,8 +71,13 @@ $(".card").click(function(){
         console.log("Wait until cardFlip program is complete");
     }
 
-    //Flip card, count number active, & if active=2, check for match
+    //Flip card & if two active, check for match
     else {
+        //Increase move counter by one & update html display
+        moveCounter += 1;
+        document.getElementById('numberMoves').innerHTML = moveCounter;
+        console.log("The number of moves is " + moveCounter);
+
         $(".back",this).removeClass("d-none");
         $(".back",this).addClass("active");
         activeCards=$('.active').length;
@@ -46,6 +89,20 @@ $(".card").click(function(){
     }
 
 });
+
+//Citation: Fisher Yates method, as conveyed by Frank Mitchell: https://www.frankmitchell.org/2015/01/fisher-yates/
+function shuffle (array) {
+    var i = 0
+      , j = 0
+      , temp = null
+  
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+  }
         
 
 function matchCheck(){
@@ -63,12 +120,6 @@ function matchCheck(){
                 $(".active").parent().addClass("matched");
                 $(".back").removeClass("active"); 
                 checkingState=false;
-
-                numberMatched=$(".matched").length;
-                if (numberMatched===16){
-                    $(".card").addClass("matched-all");
-                    console.log("You have found them all");
-                }
             }
 
             //If not a match, flip back over and remove active class
@@ -82,29 +133,41 @@ function matchCheck(){
             }
 
             //Adjust star rating
-            document.getElementById('starRating').innerHTML = starRating;
             matchesPerMove = numberMatched/moveCounter;
-            if (matchesPerMove>=0.5){
+            if (matchesPerMove>=0.6){
                 starRating=3;
-                document.getElementById('starRating').innerHTML = starRating;
+                $(".fa-star:eq(0)").addClass("star-filled");
+                $(".fa-star:eq(1)").addClass("star-filled");
+                $(".fa-star:eq(2)").addClass("star-filled");
             }
-            else if (matchesPerMove<0.5 && matchesPerMove>=0.25){
+            else if (matchesPerMove<0.6 && matchesPerMove>=0.3){
                 starRating=2;
-                document.getElementById('starRating').innerHTML = starRating;
+                $(".fa-star:eq(0)").addClass("star-filled");
+                $(".fa-star:eq(1)").addClass("star-filled");
+                $(".fa-star:eq(2)").removeClass("star-filled");
             }
-            else if (matchesPerMove<0.25){
+            else if (matchesPerMove<0.3){
                 starRating=1;
-                document.getElementById('starRating').innerHTML = starRating;
+                $(".fa-star:eq(0)").addClass("star-filled");
+                $(".fa-star:eq(1)").removeClass("star-filled");
+                $(".fa-star:eq(2)").removeClass("star-filled");
+            }
+
+            numberMatched=$(".matched").length;
+            if (numberMatched===16){
+                console.log("You have found them all");
+                clearInterval(timeVar);
+                document.getElementById("modalText").innerHTML = "Congratulations! You have matched all of the wildlife in " +  moveCounter + " moves over the course of " + seconds + " seconds, with a final rating of " + starRating + " stars!";
+                $("#winnerModal").show();
             }
 
         }, 250);
     }
 
-    //TODO: Add styling/animation for when #matched=16
+    function myTimer() {
+        seconds +=1 ; 
+        document.getElementById("timer").innerHTML = seconds;
+    }
 
-
-
-    //IDEAS:
-    //--Add start over button
-    //--Randomize font icons
-    //--Jungle theme
+    //TODO: Add modal for when all matched
+    //TODO: Jungle theme styling
